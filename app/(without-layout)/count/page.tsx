d1028b91d9e599287bdbeb40c71ec827e6601ca0
FZ1010 <farhanzare1010@gmail.com>
2024-12-05 11:43:47 -0500

    added cards

--- app/(without-layout)/count/page.tsx
+++ app/(without-layout)/count/page.tsx
@@ -1,16 +1,21 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import DrinkCard from "@/app/(without-layout)/count/DrinkCard";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      Hello
+    <section className="grid grid-cols-4 items-center justify-center gap-4 py-8 md:py-10">
+      <DrinkCard image="/photos/drinks/tequila.png" name="Tequila" />
+      <DrinkCard image="/photos/drinks/cognac.png" name="Cognac" />
+      <DrinkCard image="/photos/drinks/whiskey.png" name="Whiskey" />
+      <DrinkCard image="/photos/drinks/vodka.png" name="Vodka" />
+      <DrinkCard image="/photos/drinks/gin.png" name="Gin" />
+      <DrinkCard image="/photos/drinks/rum.jpg" name="Rum" />
+      <DrinkCard
+        image="/photos/drinks/sagi.png"
+        name="Persian Empire Arak Saggi"
+      />
+      <DrinkCard image="/photos/drinks/sake.jpg" name="Sake" />
+      <DrinkCard isBeer image="/photos/drinks/beer.png" name="Beer" />
+      <DrinkCard isWine image="/photos/drinks/wine.png" name="Wine" />
     </section>
   );
 }
