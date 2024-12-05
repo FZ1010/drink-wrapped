10e83b76b90f8d27235ce28714e18b9ee60f4078
FZ1010 <farhanzare1010@gmail.com>
2024-12-05 13:06:09 -0500

    added auth

--- app/(without-layout)/count/DrinkCard.tsx
+++ app/(without-layout)/count/DrinkCard.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Card, CardFooter, CardHeader } from "@nextui-org/card";
 import { Button } from "@nextui-org/button";
 import { Image } from "@nextui-org/image";
-import { FaGlassWhiskey, FaWineBottle } from "react-icons/fa";
+import {FaGlassWhiskey, FaWineBottle, FaWineGlassAlt} from "react-icons/fa";
 import { Tooltip } from "@nextui-org/tooltip";
+import {GiSodaCan} from "react-icons/gi";
+import {GoContainer} from "react-icons/go";
+import {LuContainer} from "react-icons/lu";
 
 interface DrinkCardProps {
   name: string;
@@ -18,6 +21,100 @@ const DrinkCard = ({
   isBeer = false,
   isWine = false,
 }: DrinkCardProps) => {
+    const rednerButtons = () => {
+        if (isBeer) {
+            return (
+                <>
+                    <Tooltip showArrow content="Add a Can" placement="top">
+                        <Button
+                            isIconOnly
+                            className="text-tiny text-white bg-black/20"
+                            color="default"
+                            radius="lg"
+                            size="lg"
+                            variant="flat"
+                        >
+                            <GiSodaCan size={22}/>
+                        </Button>
+                    </Tooltip>
+                    <Tooltip showArrow content="Add a 6 Pack" placement="top">
+                        <Button
+                            className="text-tiny text-white bg-black/20"
+                            color="default"
+                            radius="lg"
+                            size="lg"
+                            variant="flat"
+                        >
+                            <LuContainer   size={28} />
+                        </Button>
+                    </Tooltip>
+                </>
+            );
+        } else if (isWine) {
+                return (
+                    <>
+                        <Tooltip showArrow content="Add a Glass" placement="top">
+                            <Button
+                                isIconOnly
+                                className="text-tiny text-white bg-black/20"
+                                color="default"
+                                radius="lg"
+                                size="lg"
+                                variant="flat"
+                            >
+                                <FaWineGlassAlt  size={25} />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip showArrow content="Add a Bottle" placement="top">
+                            <Button
+                                className="text-tiny text-white bg-black/20"
+                                color="default"
+                                radius="lg"
+                                size="lg"
+                                variant="flat"
+                            >
+                                <FaWineBottle size={25} />
+                            </Button>
+                        </Tooltip>
+                    </>
+                );
+            }
+        else {
+            return (
+                <>
+                    <Tooltip showArrow content="Add a Shot" placement="top">
+                        <Button
+                            isIconOnly
+                            className="text-tiny text-white bg-black/20"
+                            color="default"
+                            radius="lg"
+                            size="lg"
+                            variant="flat"
+                        >
+                            <FaGlassWhiskey size={17} />
+                        </Button>
+                    </Tooltip>
+                    <Tooltip
+                        showArrow
+                        color={"danger"}
+                        content="Add a Bottle"
+                        placement="top"
+                    >
+                        <Button
+                            className="text-tiny text-white bg-black/20"
+                            color="default"
+                            radius="lg"
+                            size="lg"
+                            variant="flat"
+                        >
+                            <FaWineBottle size={25} />
+                        </Button>
+                    </Tooltip>
+                </>
+            );
+        }
+    }
+
   return (
     <Card isFooterBlurred className="border-none" radius="lg">
       <CardHeader className="absolute z-20 top-1 flex-col items-start">
@@ -26,8 +123,7 @@ const DrinkCard = ({
         </h4>
       </CardHeader>
       <Image
-        isBlurred
-        alt="Woman listing to music"
+        alt={name}
         className="object-cover"
         height={500}
         src={image}
@@ -35,34 +131,7 @@ const DrinkCard = ({
       />
       <CardFooter className="border-white/20 border-1 py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
         <div className={"flex w-full justify-between"}>
-          <Tooltip showArrow content="Add a Shot" placement="top">
-            <Button
-              isIconOnly
-              className="text-tiny text-white bg-black/20"
-              color="default"
-              radius="lg"
-              size="lg"
-              variant="flat"
-            >
-              <FaGlassWhiskey size={17} />
-            </Button>
-          </Tooltip>
-          <Tooltip
-            showArrow
-            color={"danger"}
-            content="Add a Bottle"
-            placement="top"
-          >
-            <Button
-              className="text-tiny text-white bg-black/20"
-              color="default"
-              radius="lg"
-              size="lg"
-              variant="flat"
-            >
-              <FaWineBottle size={25} />
-            </Button>
-          </Tooltip>
+            {rednerButtons()}
         </div>
       </CardFooter>
     </Card>
--- app/(without-layout)/count/page.tsx
+++ app/(without-layout)/count/page.tsx
@@ -1,8 +1,19 @@
+"use client";
 import DrinkCard from "@/app/(without-layout)/count/DrinkCard";
-
+import {GoogleLogin} from "@react-oauth/google";
 export default function Home() {
   return (
     <section className="grid grid-cols-4 items-center justify-center gap-4 py-8 md:py-10">
+        <GoogleLogin
+            onSuccess={credentialResponse => {
+                console.log(credentialResponse);
+            }}
+            onError={() => {
+                console.log('Login Failed');
+            }}
+            auto_select
+            useOneTap
+        />
       <DrinkCard image="/photos/drinks/tequila.png" name="Tequila" />
       <DrinkCard image="/photos/drinks/cognac.png" name="Cognac" />
       <DrinkCard image="/photos/drinks/whiskey.png" name="Whiskey" />
